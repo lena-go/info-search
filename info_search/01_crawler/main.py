5ea2a486609a758d02aed7b8574f2d8479b91f0c
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2024-02-23 12:41:06 +0300

    crawler and text processing

---
+++ info_search/01_crawler/main.py
@@ -0,0 +1,159 @@
+import time
+import random
+import threading
+import argparse
+from queue import Queue
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
+
+
+from user_agents import ua_list
+
+
+BASE_URL = 'https://gramota.ru'
+MIN_NUM_WORDS_PER_PAGE = 1000
+PAGES_FOLDER = 'pages'
+NUM_PAGES = 100
+
+
+parser = argparse.ArgumentParser(
+    prog='Crawler',
+    description='Crawles specified web addresses recursively',
+)
+parser.add_argument(
+    'addresses', metavar='A', type=str, nargs='+',
+    help='a web address to be crawled'
+)
+
+
+class Page:
+    def __init__(self, url, text):
+        self.url = url
+        self.text = text
+        self.i = -1
+
+    def __str__(self):
+        return f"{self.i} - {self.url}"
+
+
+def get_html(url: str) -> str:
+    headers = {'User-Agent': random.choice(ua_list)}
+    try:
+        resp = requests.get(url, headers=headers)
+        resp.encoding = resp.apparent_encoding
+        return resp.text
+    except Exception as e:
+        print(e)
+        return ''
+
+
+def crawl_page(
+        soup: BeautifulSoup,
+        visited_urls: Queue,
+        urls: Queue
+) -> None:
+    link_elements = soup.select('a[href]')
+    for link_element in link_elements:
+        url = link_element['href']
+        if url == '/':
+            continue
+        if not url.startswith(('https://', 'http://')):
+            url = BASE_URL + url
+        if (
+                url not in visited_urls.queue
+                and url not in urls.queue
+                and not url.startswith('javascript')
+        ):
+            urls.put(url)
+
+
+def get_content(soup: BeautifulSoup, url: str, pages: Queue) -> bool:
+    text = soup.get_text()
+    tokenized = word_tokenize(text)
+    if len(tokenized) > MIN_NUM_WORDS_PER_PAGE:
+        pages.put(Page(url, text))
+        return True
+    return False
+
+
+def download_pages(
+        urls: Queue,
+        visited_urls: Queue,
+        pages: Queue,
+        num_pages: int,
+        lock: threading.Lock,
+) -> None:
+    while not urls.empty() and num_pages < NUM_PAGES:
+        current_url = urls.get()
+        visited_urls.put(current_url)
+        print(current_url)
+        soup = BeautifulSoup(get_html(current_url), 'html.parser')
+        crawl_page(soup, visited_urls, urls)
+        urls.task_done()
+        if get_content(soup, current_url, pages):
+            lock.acquire()
+            num_pages += 1
+            lock.release()
+
+
+def save_index(pages: Queue) -> None:
+    path = Path('..', 'index.txt')
+    with path.open(mode='w', encoding='utf-8') as f:
+        f.writelines(map(
+            lambda page: f"{PAGES_FOLDER}/{page.i}.txt {page.url}\n",
+            pages.queue
+        ))
+
+
+def save_page(page: Page):
+    path = Path('..', PAGES_FOLDER, str(page.i) + '.txt')
+    with path.open(mode='w', encoding='utf-8') as f:
+        f.write(page.text)
+
+
+def save_pages(pages: Queue):
+    folder = Path('..', PAGES_FOLDER)
+    if not folder.is_dir():
+        folder.mkdir()
+    while not pages.empty():
+        save_page(pages.get())
+
+
+def run(addresses: [str]):
+    urls = Queue()
+    urls.put(BASE_URL)
+    visited_urls = Queue()
+    pages = Queue()
+    num_pages = 0
+    lock = threading.Lock()
+
+    num_workers = 4
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        for _ in range(num_workers):
+            executor.submit(
+                download_pages,
+                urls,
+                visited_urls,
+                pages,
+                num_pages,
+                lock,
+            )
+
+    for i, page in enumerate(pages.queue):
+        page.i = i
+
+    save_index(pages)
+
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        for _ in range(num_workers):
+            executor.submit(save_pages, pages)
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    run(parser.parse_args().addresses)
+    print("--- %s seconds ---" % (time.time() - start_time))
---
+++ info_search/02_text_processing/main.py
@@ -0,0 +1,103 @@
+import time
+from pathlib import Path
+from queue import Queue
+from concurrent.futures import ThreadPoolExecutor
+import string
+import threading
+
+import nltk
+from nltk.corpus import stopwords
+from natasha import (
+    Segmenter,
+    MorphVocab,
+    NewsEmbedding,
+    NewsMorphTagger,
+    Doc
+)
+
+
+INDEX_NAME = 'index.txt'
+PROCESSED_PAGES_FOLDER = 'processed_pages'
+
+
+def read_index() -> Queue:
+    index = Path('..', INDEX_NAME)
+    with index.open(encoding='utf-8') as f:
+        files = Queue()
+        for line in f:
+            files.put(line.split()[0])
+    return files
+
+
+class PageProcessor:
+    def __init__(self, processed_pages_path: Path):
+        self.processed_pages_path = processed_pages_path
+        self.lock = threading.Lock()
+
+        self.segmenter = Segmenter()
+        self.morph_vocab = MorphVocab()
+        self.morph_tagger = NewsMorphTagger(NewsEmbedding())
+
+        self.punctuation = list(string.punctuation)
+        self.punctuation.remove('-')
+        self.punctuation.remove('.')
+        self.punctuation += [' - ', '–', '—', '«', '»']
+
+    def process_page(self, filename: str):
+        page_path = Path('..', filename)
+        with page_path.open(encoding='utf-8') as f:
+            page = f.read()
+        for symbol in self.punctuation:
+            page = page.replace(symbol, '')
+
+        doc = Doc(page)
+
+        self.lock.acquire()
+        doc.segment(self.segmenter)
+        doc.tag_morph(self.morph_tagger)
+        for token in doc.tokens:
+            token.lemmatize(self.morph_vocab)
+        self.lock.release()
+
+        lemmatized_text = ' '.join([
+            token.lemma for token in doc.tokens
+            if token.lemma not in stopwords.words('russian')
+        ])
+        # print(lemmatized_text)
+        self.save_processed_page(filename.split('/')[1], lemmatized_text)
+
+    def process_pages(self, files: Queue):
+        while not files.empty():
+            file_name = files.get()
+            files.task_done()
+            self.process_page(file_name)
+
+    def save_processed_page(self, filename: str, text: str):
+        path = self.processed_pages_path / filename
+        with path.open(mode='w', encoding='utf-8') as f:
+            f.write(text)
+            f.write('\n')
+        print(f"{path} was saved")
+
+
+def make_folder(folder_path: Path):
+    if not folder_path.is_dir():
+        folder_path.mkdir()
+
+
+def run():
+    files = read_index()
+    processed_pages_path = Path('..', PROCESSED_PAGES_FOLDER)
+    make_folder(processed_pages_path)
+    pp = PageProcessor(processed_pages_path)
+    num_workers = 4
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        for i in range(num_workers):
+            executor.submit(pp.process_pages, files)
+
+
+if __name__ == '__main__':
+    nltk.download('stopwords')
+    start_time = time.time()
+    run()
+    print("--- %s seconds ---" % (time.time() - start_time))
