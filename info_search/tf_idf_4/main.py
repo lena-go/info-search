0366e09828747391237d0be0fe7a78312305a617
Elena Gordeeva <gordeewa29@gmail.com>
2024-03-24 14:39:00 +0300

    calculation of ITF

--- info_search/tf_idf_4/main.py
+++ info_search/tf_idf_4/main.py
@@ -1,63 +1,28 @@
-from tabulate import tabulate
+import time
 
 from info_search.inverted_index_3.index import (
     load_inv_index_from_file,
     PageReader,
-    Page,
 )
-
-
-def round_doc_vecs(doc_vecs: [{str: int}], max_signs: int):
-    for vec in doc_vecs:
-        for word, val in vec.items():
-            if val > 0:
-                vec[word] = round(vec[word], max_signs)
-
-
-def calc_tf(
-        docs: [Page],
-        lexicon_volume: int,
-        initial_doc_vec: {str: int},
-        do_round: bool = True,
-        max_signs: int = 5,
-) -> [{str: int}]:
-
-    tf_vecs = []
-    for i, doc in enumerate(docs):
-        doc_vec = initial_doc_vec.copy()
-        for word in doc.words:
-            try:
-                doc_vec[word] += 1 / lexicon_volume
-            except KeyError:
-                print(f'No word {word}. Update inverted index')
-        tf_vecs.append(doc_vec)
-
-    if do_round:
-        round_doc_vecs(tf_vecs, max_signs=max_signs)
-
-    return tf_vecs
-
-
-def stringify_vecs(doc_vecs: [{str: int}]) -> str:
-    table_headers = list(doc_vecs[0].keys())
-    rows = [vec.values() for vec in doc_vecs]
-    return tabulate(rows, headers=table_headers, showindex=True)
-
-
-def save_table(table: str, table_name: str) -> None:
-    with open(table_name, 'w', encoding='utf-8') as f:
-        f.write(table)
+from info_search.tf_idf_4.tf import TFVec
+from info_search.tf_idf_4.idf import IDFVec
 
 
 def run():
     inv_index = load_inv_index_from_file()
     docs = PageReader().parse_pages()
     lexicon_volume = len(inv_index)
-    initial_doc_vec = {k: 0 for k in inv_index.keys()}
 
-    tf_vecs = calc_tf(docs, lexicon_volume, initial_doc_vec)
-    save_table(stringify_vecs(tf_vecs), 'tf.txt')
+    tf_vecs = TFVec()
+    tf_vecs.calc_tf(docs, lexicon_volume, inv_index)
+    tf_vecs.save_as_table()
+
+    idf_vec = IDFVec()
+    idf_vec.calc_idf(inv_index)
+    idf_vec.save_as_table()
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     run()
+    print("--- %s seconds ---" % (time.time() - start_time))
